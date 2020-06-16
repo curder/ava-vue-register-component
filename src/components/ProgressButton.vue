56bc0e44279de4e0809611e91d7ba8709dcbf1b1
curder <curder@foxmail.com>
2020-06-16 10:24:50 +0800

    fixed multiple request when click the submit and send sms verify button

--- src/components/ProgressButton.vue
+++ src/components/ProgressButton.vue
@@ -1,15 +1,17 @@
 <template>
-    <button class="relative">
-        <span :class="{ 'invisible': processing }">
-            <slot/>
-        </span>
+  <button class="relative" :disabled="processing">
+    <span :class="{ 'invisible': processing }">
+      <slot />
+    </span>
 
-        <span v-if="processing"
-              class="absolute"
-              style="top: 50%; left: 50%; transform: translate(-50%, -50%)">
-            <loader width="32"/>
-        </span>
-    </button>
+    <span
+      v-if="processing"
+      class="absolute"
+      style="top: 50%; left: 50%; transform: translate(-50%, -50%)"
+    >
+      <loader width="32" />
+    </span>
+  </button>
 </template>
 
 <script>
