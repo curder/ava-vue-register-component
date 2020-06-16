7f7c0e99fba4af0cda763238cdffa85bb0ac202e
curder <curder@foxmail.com>
2020-06-16 10:23:25 +0800

    add v-for key bind

--- examples/pages/SmsRegister.vue
+++ examples/pages/SmsRegister.vue
@@ -11,100 +11,137 @@
       @register-successful="registerSuccessfulHandle"
       ref="validateLogic"
     >
-      <div class="p-8 border rounded bg-white"
+      <div
+        class="p-8 border rounded bg-white"
         slot-scope="{ form, isProcessing, formSubmitHandle, verifyCodeSendHandle, verifyCodeForm, verifyCodeCanBeSend, verifyCodeTotalTimer, verifyCodeIsProcessing }"
-        v-cloak>
+        v-cloak
+      >
         <h2 class="text-center text-2xl font-semibold font-sans text-gray-600">手机号注册</h2>
-        <form class="mt-2 text-gray-900"
+        <form
+          class="mt-2 text-gray-900"
           @submit.prevent="formSubmitHandle"
           @keydown="form.errors.clear($event.target.name)"
-          @keyup="form.errors.clear($event.target.name)">
+          @keyup="form.errors.clear($event.target.name)"
+        >
           <div class="form-field mb-4">
-            <input type="text"
+            <input
+              type="text"
               name="name"
               class="h-10 w-full border border-gray-300 rounded px-3 py-1 hover:bg-white outline-none focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-lg"
               v-model="form.name"
               :class="{'bg-red-100 border-red-300 focus:border-red-300': form.errors.has('name')}"
-              placeholder="姓名"/>
-            <div v-if="form.errors.has('name')"
+              placeholder="姓名"
+            />
+            <div
+              v-if="form.errors.has('name')"
               v-text="form.errors.first('name')"
               class="form-field-error -mb-4 text-left text-xs text-red-500 truncate"
             ></div>
           </div>
           <div class="form-field mb-4">
-            <input type="text"
+            <input
+              type="text"
               ref="phone"
               name="phone"
               class="h-10 w-full border border-gray-300 rounded px-3 py-1 hover:bg-white outline-none focus:border-gray-500 focus:bg-white"
               v-model="form.phone"
               :class="{ 'bg-red-100 border-red-300 focus:border-red-300': form.errors.has('phone') || verifyCodeForm.errors.has('phone') }"
-              placeholder="手机号码" />
-            <div v-if="form.errors.has('phone') || verifyCodeForm.errors.has('phone')"
+              placeholder="手机号码"
+            />
+            <div
+              v-if="form.errors.has('phone') || verifyCodeForm.errors.has('phone')"
               v-text=" form.errors.first('phone') || verifyCodeForm.errors.first('phone')"
-              class="form-field-error -mb-4 text-left text-xs text-red-500 truncate"></div>
+              class="form-field-error -mb-4 text-left text-xs text-red-500 truncate"
+            ></div>
           </div>
           <div class="form-field mb-4">
             <div class="flex items-center justify-center items-center border border-gray-300">
-              <input type="text"
+              <input
+                type="text"
                 name="verify_code"
                 class="h-10 w-1/2 rounded-l px-3 py-1 hover:bg-white outline-none focus:border-gray-500 focus:bg-white"
                 v-model="form.verify_code"
                 :class="{'bg-red-100 border-red-300 focus:border-red-300': form.errors.has('verify_code')}"
-                placeholder="验证码" />
-              <progress-button type="button"
+                placeholder="验证码"
+              />
+              <progress-button
+                type="button"
                 v-if="verifyCodeCanBeSend"
                 :processing="verifyCodeIsProcessing"
                 @click.native="verifyCodeSendHandle"
-                class="cursor-pointer w-1/2 py-1 rounded-r underline focus:outline-none text-999 text-blue-600 text-center">获取验证码</progress-button>
-              <progress-button type="button"
+                class="cursor-pointer w-1/2 py-1 rounded-r underline focus:outline-none text-999 text-blue-600 text-center"
+              >获取验证码</progress-button>
+              <progress-button
+                type="button"
                 v-else
-                class="w-1/2 py-1 rounded-r focus:outline-none cursor-not-allowed text-999 text-xs text-center">
-                <span v-text="verifyCodeTotalTimer"></span>秒后重新获取</progress-button>
+                class="w-1/2 py-1 rounded-r focus:outline-none cursor-not-allowed text-999 text-xs text-center"
+              >
+                <span v-text="verifyCodeTotalTimer"></span>秒后重新获取
+              </progress-button>
             </div>
-            <div v-if="form.errors.has('verify_code')"
+            <div
+              v-if="form.errors.has('verify_code')"
               v-text="form.errors.first('verify_code')"
-              class="form-field-error -mb-4 text-left text-xs text-red-500 truncate"></div>
+              class="form-field-error -mb-4 text-left text-xs text-red-500 truncate"
+            ></div>
           </div>
           <div class="form-field mb-4">
-            <input type="text"
+            <input
+              type="text"
               v-model="form.email"
               :class="{ 'bg-red-100 border-red-300 focus:border-red-300': form.errors.has('email')}"
               class="h-10 w-full border border-gray-300 rounded px-3 py-1 hover:bg-white outline-none focus:border-gray-500 focus:bg-white"
-              placeholder="邮箱"/>
-            <div v-if="form.errors.has('email')"
+              placeholder="邮箱"
+            />
+            <div
+              v-if="form.errors.has('email')"
               v-text="form.errors.first('email')"
-              class="form-field-error -mb-4 text-left text-xs text-red-500 truncate"></div>
+              class="form-field-error -mb-4 text-left text-xs text-red-500 truncate"
+            ></div>
           </div>
 
           <div class="form-field mb-2">
-            <input type="text"
+            <input
+              type="text"
               v-model="form.extra.account"
               :class="{'bg-red-100 border-red-300 focus:border-red-300': form.errors.has('extra.account')}"
               class="h-10 w-full border border-gray-300 rounded px-3 py-1 hover:bg-white outline-none focus:border-gray-500 focus:bg-white"
-              placeholder="交易账号"/>
-            <div v-if="form.errors.has('extra.account')"
+              placeholder="交易账号"
+            />
+            <div
+              v-if="form.errors.has('extra.account')"
               v-text="form.errors.first('extra.account')"
               class="form-field-error -mb-4 text-left text-xs text-red-500 truncate"
             ></div>
           </div>
 
           <div class="form-field mb-2">
-            <select v-model="form.source"
+            <select
+              v-model="form.source"
               :class="{'bg-red-100 border-red-300 focus:border-red-300': form.errors.has('source')}"
-              class="h-10 w-full border border-gray-300 rounded px-3 py-1 hover:bg-white outline-none focus:border-gray-500 focus:bg-white">
-              <option value="">何种渠道了解爱华</option>
-              <option v-for="source in sources" value="source" v-text="source"></option>
+              class="h-10 w-full border border-gray-300 rounded px-3 py-1 hover:bg-white outline-none focus:border-gray-500 focus:bg-white"
+            >
+              <option value>何种渠道了解爱华</option>
+              <option
+                v-for="(source, index) in sources"
+                :key="index"
+                value="source"
+                v-text="source"
+              ></option>
             </select>
-            <div v-if="form.errors.has('source')"
+            <div
+              v-if="form.errors.has('source')"
               v-text="form.errors.first('source')"
               class="form-field-error -mb-4 text-left text-xs text-red-500 truncate"
             ></div>
           </div>
 
           <div class="form-button pt-2">
-            <progress-button type="submit"
+            <progress-button
+              type="submit"
               class="h-10 tracking-widest w-full text-white px-10 rounded py-1 bg-form-button bg-f89 focus:outline-none bg-green-500 hover:bg-green-400"
-              :processing="isProcessing">提交</progress-button>
+              :processing="isProcessing"
+            >提交</progress-button>
           </div>
         </form>
       </div>
@@ -119,7 +156,7 @@ export default {
   mixins: [sources],
   data() {
     return {
-      type: "v9t2AOe86aCI",
+      type: "v8gAomLh6w46",
       name: "测试"
     };
   },
